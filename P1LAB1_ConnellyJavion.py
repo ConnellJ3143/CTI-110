b86d4351fc803c353c591085424b4ce94e6d728d
ConnellJ3143 <Javion.connelly@gmail.com>
2026-09-10 11:13:02 -0400

    Add files via upload

---
+++ P1LAB1_ConnellyJavion.py
@@ -0,0 +1,12 @@
+# Javion Connelly
+# 9/10/26
+# Inputs and outputs
+
+# Get the first name
+first_name = input("Enter First Name: ")
+
+# Get last name
+Last_name = input("Enter Last Name: ")
+
+# Output welcome message
+print("Hello,", first_name, Last_name + "!", "Welcome to CTI-110")
